030d11578de9c724305da78a409c5c57ce1827fe
Arsha220 <arshasjeevan75@gmail.com>
2021-09-29 10:17:00 +0530

    commit

--- js/todo.js
+++ js/todo.js
@@ -1,40 +1,3 @@
-// let user=document.getElementById("user");
-// let pwd=document.getElementById("pwd");
-// let form=document.getElementsByClassName("form-container");
-// function validate(callback){
-//     if (user.value.trim()=="admin"&&pwd.value==12345){
-//         return callback();
-//     }
-//     else if(user.value!="admin"&&pwd.value==12345){
-//         error.innerHTML=" Username is Invalid";
-//         error.style.border="3px solid red";
-//         error.style.color="yellow";
-//         error1.innerHTML=" Password is Valid";
-//         error1.style.border="3px solid blue";
-//         error1.style.color="yellow";
-//     }
-//     else if(user.value.trim()=="admin"&&pwd.value!=12345){
-//         error.innerHTML=" Username is valid";
-//         error.style.border="3px solid blue";
-//         error.style.color="yellow";
-//         error1.innerHTML=" Password is Invalid";
-//         error1.style.border="3px solid red";
-//         error1.style.color="yellow"
-//         return false;
-//     }
-//     else{
-//         error1.innerHTML=" Username and Password is Invalid";
-//         error1.style.border="3px solid red";
-//         error1.style.color="yellow";
-//         return false;
-//     }
-//     function callback()
-//     {
-//         error1.innerHTML=" Username and Password is Valid";
-//         error1.style.border="3px solid blue";
-//         error1.style.color="yellow";
-//     }  
-// }
 let user=document.getElementById("user");
 let pwd=document.getElementById("pwd");
 let user1=document.getElementById("user1");
@@ -43,7 +6,6 @@ function usercheck() {
     if (user.value == "admin") {
         console.log(user.value);
         user1.innerHTML = "";
-        pwd1.innerHTML = "";
         return true;
     } else {
         user1.innerHTML = "Username:admin";
@@ -54,11 +16,12 @@ function usercheck() {
 }
 function passcheck() {
     if (pwd.value == "12345") {
+        console.log(pwd.value);
         pwd1.innerHTML = "";
         return true;
     } 
     else {
-        pwd1.innerHTML = "Password : 12345";
+        pwd1.innerHTML = "Password:12345";
         pwd1.style.color = "red";
         pwd1.style.fontFamily="San serif";
         return false;
